dc799f6171d2855abef1d73452b6d3414097f39a
Hannah Steinbach <hannah.steinbach@uni-potsdam.de>
2024-06-19 16:17:28 +0200

    add keywords

--- django/dreamcatcher/dreams/models.py
+++ django/dreamcatcher/dreams/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from keybert import KeyBERT
+import json
 
 
 class Dream(models.Model):
@@ -9,12 +11,30 @@ class Dream(models.Model):
     shared = models.BooleanField(default=False)
     processed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+    keywords = models.JSONField(default=list, blank=True)  # Use JSONField to store keywords as a list
+
+    def save(self, *args, **kwargs):
+        # Ensure content is a string
+        content_str = str(self.content)
+
+        # Extract keywords using KeyBERT
+        kw_model = KeyBERT()
+        keywords = kw_model.extract_keywords(content_str, keyphrase_ngram_range=(1, 1), stop_words=None)
+
+        # Store only the keywords as a list of strings
+        self.keywords = [keyword[0] for keyword in keywords]
+
+        # Save the instance
+        super().save(*args, **kwargs)
 
     def likes_count(self):
         return self.likes.count()
 
     def liked_users(self):
         return [like.user for like in self.likes.all()]
+
+    def __str__(self):
+        return f"{self.date}: {self.content[:50]}"
 
     def __str__(self):
         return f"{self.date}: {self.content[:50]}"
