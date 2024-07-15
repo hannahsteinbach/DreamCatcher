885f2af0755dd7b17d3ff55cb3cad3b6f76a0b72
Hannah Steinbach <hannah.steinbach@uni-potsdam.de>
2024-07-15 20:10:06 +0200

    last one i swear

--- django/dreamcatcher/dreams/views.py
+++ django/dreamcatcher/dreams/views.py
@@ -590,7 +590,8 @@ class CustomPasswordResetView(PasswordResetView):
 def home_logged_in(request):
     user = request.user
     dreams = Dream.objects.filter(user=user)
-    show_tutorial = user.is_new
+    # show_tutorial = user.is_new
+    show_tutorial = True # just put this as true as a default for testing
     if show_tutorial:
         user.is_new = False
         user.save()
